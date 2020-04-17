bb44eaef6d69f97412fc490430a434f603c54355
Elliot Blanchard <public@elliotblanchard.com>
2020-04-17 19:17:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,9 +11,9 @@ class Song
 
     @@count += 1
 
-    if !@@genres.include? @genre
+    #if !@@genres.include? @genre
       @@genres << @genre
-    end
+    #end
   end
 
   def self.count
@@ -21,7 +21,7 @@ class Song
   end
 
   def self.genres
-    @@genres
+    @@genres.uniq
   end
 
 end
