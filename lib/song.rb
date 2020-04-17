1e14cc9b633b43970fd5c755b57633d97f072132
Elliot Blanchard <public@elliotblanchard.com>
2020-04-17 19:11:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@genres = []
 
   def initialize(name,artist,genre)
     @name = name
@@ -9,10 +10,15 @@ class Song
     @genre = genre
 
     @@count += 1
+    @@genres << genre
   end
 
   def self.count
     @@count
+  end
+
+  def self.genres
+    @@genres
   end
 
 end
